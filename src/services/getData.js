c14c3156268e234cd7ba6fb8165d2ff5a78d37aa
betoje <betoje@gmail.com>
2023-08-29 07:47:24 -0500

    getData.js fetch address problem

--- src/services/getData.js
+++ src/services/getData.js
@@ -1,9 +1,9 @@
-const API_URL = "src";
+const API_URL = "https://api-url";
 
 export async function getData() {
   try {
-    const response = await fetch(`${API_URL}/assets/movies.json`);
-    // const response = await fetch(`src/assets/movies.json`);
+    // const response = await fetch(`${API_URL}/x`);
+    const response = await fetch('src/assets/movies.json');
     const json = await response.json();
     return json;
   } catch (error) {
