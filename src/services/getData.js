5a22c2d7e2ac5aa687e39d52e4850c9bc954f408
betoje <betoje@gmail.com>
2023-08-29 18:26:56 -0500

    movies.json a public folder

--- src/services/getData.js
+++ src/services/getData.js
@@ -3,7 +3,8 @@ const API_URL = "https://api-url";
 export async function getData() {
   try {
     // const response = await fetch(`${API_URL}/x`);
-    const response = await fetch('src/assets/movies.json');
+    // const response = await fetch('src/assets/movies.json');
+    const response = await fetch('movies.json');
     const json = await response.json();
     return json;
   } catch (error) {
